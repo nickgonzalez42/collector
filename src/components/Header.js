b3703f473306697c547ea5ef00aaa53db59167d9
Nicholas Gonzalez <thisnickg194@gmail.com>
2024-01-01 10:53:58 -0600

    header updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import { useAuthenticator } from "@aws-amplify/ui-react";
 import { Navbar, Button } from "flowbite-react";
-import { CustomFlowbiteTheme } from "flowbite-react";
 
 const customTheme = {
   color: {
@@ -16,7 +15,7 @@ export function Header() {
   }
 
   return (
-    <Navbar fluid className="text-white border-[#a48b47] bg-[#262647]">
+    <Navbar fluid className="text-white bg-[#262647]">
       <Navbar.Brand href="/">
         {/* <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" /> */}
         <span className="self-center whitespace-nowrap text-m l:text-xl font-semibold dark:text-white">
@@ -24,30 +23,32 @@ export function Header() {
         </span>
       </Navbar.Brand>
       {/* <div className="flex mr-2 ml-auto sticky md:order-2"></div> */}
-      <Navbar.Toggle />
-      <Navbar.Collapse className="md:ml-8 ml-0">
-        <Button theme={customTheme} color="primary" className="mt-1" href="/cards">
-          cards
-        </Button>
-        <Button theme={customTheme} color="primary" className="mt-1" href="/collection">
-          collection
-        </Button>
-        <Button disabled={true} theme={customTheme} color="primary" className="mt-1" href="/decks">
-          view decks
-        </Button>
-        <Button disabled={true} theme={customTheme} color="primary" className="mt-1" href="/builder">
-          create deck
-        </Button>
-        {route !== "authenticated" ? (
-          <Button theme={customTheme} color="primary" href="/login">
-            login
+      <div className="flex flex-initial justify-end">
+        <Navbar.Toggle />
+        <Navbar.Collapse className="l:ml-8 ml-0">
+          <Button theme={customTheme} color="primary" className="mt-1" href="/cards">
+            cards
           </Button>
-        ) : (
-          <Button theme={customTheme} color="primary" onClick={() => logOut()}>
-            logout
+          <Button theme={customTheme} color="primary" className="mt-1" href="/collection">
+            collection
           </Button>
-        )}
-      </Navbar.Collapse>
+          <Button disabled={true} theme={customTheme} color="primary" className="mt-1" href="/decks">
+            view decks
+          </Button>
+          <Button disabled={true} theme={customTheme} color="primary" className="mt-1" href="/builder">
+            create deck
+          </Button>
+          {route !== "authenticated" ? (
+            <Button theme={customTheme} color="primary" href="/login">
+              login
+            </Button>
+          ) : (
+            <Button theme={customTheme} color="primary" onClick={() => logOut()}>
+              logout
+            </Button>
+          )}
+        </Navbar.Collapse>
+      </div>
     </Navbar>
   );
 }
