00e65ed62a8672ffc24ccddc2b4e2bd22995e22c
Nicholas Gonzalez <thisnickg194@gmail.com>
2024-01-09 15:47:15 -0600

    removed console logs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import { useAuthenticator } from "@aws-amplify/ui-react";
 import { Navbar, Button } from "flowbite-react";
-import { useEffect } from "react";
 
 const customTheme = {
   color: {
@@ -9,39 +8,22 @@ const customTheme = {
 };
 
 export function Header() {
-  // const { route, signOut, user } = useAuthenticator((context) => [context.route, context.signOut, context.user]);
   const auth = useAuthenticator((context) => [context.route, context.signOut, context.user]);
   const { signOut, authStatus } = auth;
 
-  useEffect(() => {
-    console.log(auth);
-  }, [auth]);
-
-  function test() {
-    console.log(auth);
-  }
   function logOut() {
     signOut();
   }
   return (
     <Navbar fluid className="text-white bg-[#262647]">
       <Navbar.Brand href="/">
-        {/* <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" /> */}
         <span className="self-center whitespace-nowrap text-m l:text-xl font-semibold dark:text-white">
           STRAW HAT STASH
         </span>
       </Navbar.Brand>
-      {/* <div className="flex mr-2 ml-auto sticky md:order-2"></div> */}
       <div className="flex flex-initial justify-end">
         <Navbar.Toggle />
         <Navbar.Collapse className="l:ml-8 ml-0">
-          <Button
-            onClick={() => {
-              test();
-            }}
-          >
-            test
-          </Button>
           <Button theme={customTheme} color="primary" className="mt-1" href="/cards">
             cards
           </Button>
