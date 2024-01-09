5d955cf67bb6f676faba39efc0019560b3ffe5ab
Nicholas Gonzalez <thisnickg194@gmail.com>
2024-01-09 13:04:08 -0600

    further fixing of navigation after login

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,12 +8,13 @@ import { useNavigate, useLocation } from "react-router";
 
 export function Login() {
   const { route } = useAuthenticator((context) => [context.route]);
-  const location = useLocation();
+  // const location = useLocation();
   const navigate = useNavigate();
-  let from = location.state?.from?.pathname || "/";
+  let from = "/";
+  // let from = location.state?.from?.pathname || "/";
   useEffect(() => {
     if (route === "authenticated") {
-      navigate(from, { replace: false });
+      navigate(from);
     }
   }, [route, navigate, from]);
   return (
